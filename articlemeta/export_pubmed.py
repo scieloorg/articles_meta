8dd29e0c540685e308f01456edcaad2963e085ef
Ednilson <ednilson.gesseff@scielo.org>
2014-04-29 18:23:42 -0300

    Adicinada a classe XMLJournalPipe e seu teste.

--- articlemeta/export_pubmed.py
+++ articlemeta/export_pubmed.py
@@ -4,10 +4,44 @@ import xml.etree.ElementTree as ET
 import plumber
 
 
-class SetupArticlePipe(plumber.Pipe):
+class SetupArticleSetPipe(plumber.Pipe):
 
     def transform(self, data):
 
         xml = ET.Element('ArticleSet')
 
         return data, xml
+
+
+class XMLArticlePipe(plumber.Pipe):
+
+    def tranform(self, data):
+        raw, xml = data
+
+        article = ET.Element('Article')
+
+        xml.append(article)
+
+        return data
+
+
+class XMLJournalPipe(plumber.Pipe):
+    def transform(self, data):
+        raw, xml = data
+
+        journal = ET.Element('Journal')
+        journal.text = raw.journal_title
+
+        xml.find('./Article').append(journal)
+
+        return data
+
+
+class XMLClosePipe(plumber.Pipe):
+
+    def transform(self, data):
+        raw, xml = data
+
+        data = ET.tostring(xml, encoding="utf-8", method="xml")
+
+        return data
--- tests/test_export_pubmed.py
+++ tests/test_export_pubmed.py
@@ -14,3 +14,30 @@ from articlemeta import export
 class ExportTests(unittest.TestCase):
 
     def setUp(self):
+        self._raw_json = json.loads(open(os.path.dirname(__file__)+'/fixtures/article_meta.json').read())
+        self._article_meta = Article(self._raw_json)
+
+    def test_xmlclose_pipe(self):
+
+        pxml = ET.Element('ArticleSet')
+        pxml.append(ET.Element('Article'))
+
+        data = [None, pxml]
+
+        xmlarticle = export_pubmed.XMLClosePipe()
+        xml = xmlarticle.transform(data)
+
+        self.assertEqual('<ArticleSet><Article /></ArticleSet>', xml)
+
+    def test_xmljournal_pipe(self):
+        pxml = ET.Element('ArticleSet')
+        pxml.append(ET.Element('Article'))
+
+        data = [self._article_meta, pxml]
+
+        xmlarticle = export_pubmed.XMLJournalPipe()
+        raw, xml = xmlarticle.transform(data)
+
+        journal = xml.find('./Article/Journal').text
+
+        self.assertEqual(u'Revista de Saúde Pública', journal)
