62c40aab72fd930cf159d050c39641bfa137b433
fabiobatalha <fabiobatalha@gmail.com>
2015-11-12 11:21:58 -0200

    ajustes em setup

--- setup.py
+++ setup.py
@@ -13,7 +13,8 @@ requires = [
     'uuid>=1.30',
     'pymongo>=2.5.2',
     'lxml>=3.4.2',
-    'requests>=2.6.0'
+    'requests>=2.6.0',
+    'picles.plumber>=0.10',
     'pyramid',
     'pyramid_chameleon',
     'pyramid_debugtoolbar',
