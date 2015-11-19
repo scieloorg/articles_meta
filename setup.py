05da4147e85da1408ffc4590eaa110f583cd9a52
fabiobatalha <fabiobatalha@gmail.com>
2015-11-19 14:47:42 -0200

    Ajustes de versão e dependencias

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ test_requires = ['mocker']
 
 setup(
     name="articlemeta",
-    version='0.2.45',
+    version='0.2.46',
     description="A SciELO API to load SciELO Articles metadata",
     author="SciELO",
     author_email="scielo-dev@googlegroups.com",
