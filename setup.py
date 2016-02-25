d3b37891a7224dc650a8f91776152aea34749d9a
fabiobatalha <fabiobatalha@gmail.com>
2016-02-25 14:21:56 -0300

    Ajuste nas versões do requirements

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ test_requires = ['mocker']
 
 setup(
     name="articlemeta",
-    version='0.3.50',
+    version='0.3.1',
     description="A SciELO API to load SciELO Articles metadata",
     author="SciELO",
     author_email="scielo-dev@googlegroups.com",
