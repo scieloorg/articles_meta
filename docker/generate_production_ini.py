4ad4ff19a6110f3154ff1bb81c199d694e41473b
fabiobatalha <fabiobatalha@gmail.com>
2016-08-24 16:10:30 -0300

    Ajustes para docker

--- docker/generate_production_ini.py
+++ docker/generate_production_ini.py
@@ -15,9 +15,8 @@ new_production_ini_filepath = os.path.join(PROJECT_PATH + '/production.ini')
 
 config = ConfigParser()
 config.read(production_template_ini_filepath)
-config.set('app:main', 'mongo_uri', 'mongodb://{mongodb_host}/articlemeta' % os.env.get('MONGODB_HOST', '127.0.0.1:27017'))
+config.set('app:main', 'mongo_uri', 'mongodb://{mongodb_host}/articlemeta' % os.environ.get('MONGODB_HOST', '127.0.0.1:27017'))
 config.set('app:main', 'admintoken', os.environ.get('ADMIN_TOKEN', uuid.uuid4().hex))
-
 config.set('server:main', 'port', 8000)
 
 with open(new_production_ini_filepath, 'w') as configfile:    # save
