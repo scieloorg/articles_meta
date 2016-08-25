22da7b43fb1e31dcad935ebee33dc0cf4d5a7125
fabiobatalha <fabiobatalha@gmail.com>
2016-08-25 14:00:04 -0300

    Ajustando versão de tag

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ test_requires = ['mocker']
 
 setup(
     name="articlemeta",
-    version='0.11.2',
+    version='0.12.2',
     description="A SciELO API to load SciELO Articles metadata",
     author="SciELO",
     author_email="scielo-dev@googlegroups.com",
