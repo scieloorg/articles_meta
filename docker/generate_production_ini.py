79874040f2341acee0cd3b25ea0d45a045767847
fabiobatalha <fabiobatalha@gmail.com>
2016-08-25 12:00:25 -0300

    Ajustes em nome de console_script para ficar padronizado com outras aplicações

--- docker/generate_production_ini.py
+++ docker/generate_production_ini.py
@@ -1,23 +1,20 @@
-# -*- coding: utf-8 -*-
+# coding: utf-8
 import os
 import uuid
 
-try:
-    from configparser import ConfigParser
-except ImportError:
-    from ConfigParser import ConfigParser  # ver. < 3.0
+from configparser import ConfigParser
 
 
 PROJECT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
-production_template_ini_filepath = os.path.join(PROJECT_PATH + '/production-TEMPLATE.ini')
+production_template_ini_filepath = os.path.join(PROJECT_PATH + '/production.ini-TEMPLATE')
 new_production_ini_filepath = os.path.join(PROJECT_PATH + '/production.ini')
 
 config = ConfigParser()
 config.read(production_template_ini_filepath)
 config.set('app:main', 'mongo_uri', 'mongodb://%s/articlemeta' % os.environ.get('MONGODB_HOST', '127.0.0.1:27017'))
 config.set('app:main', 'admintoken', os.environ.get('ADMIN_TOKEN', uuid.uuid4().hex))
-config.set('server:main', 'port', 8000)
+config.set('server:main', 'port', '8000')
 
 with open(new_production_ini_filepath, 'w') as configfile:    # save
     config.write(configfile)
