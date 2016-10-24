771ad2fb454f563a6f35d9495f2738ca3d2d2f65
fabiobatalha <fabiobatalha@gmail.com>
2016-10-24 17:23:33 -0200

    Ajustes em arquivos de conf

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ test_requires = ['mocker']
 
 setup(
     name="articlemeta",
-    version='0.14.3',
+    version='1.15.3',
     description="A SciELO API to load SciELO Articles metadata",
     author="SciELO",
     author_email="scielo-dev@googlegroups.com",
