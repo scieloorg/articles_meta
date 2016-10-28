6c38bb133b8a142de6ac698fd7543ac4cc8a61e2
fabiobatalha <fabiobatalha@gmail.com>
2016-10-28 15:24:05 -0200

    correção em thrift file

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ test_requires = ['mocker']
 
 setup(
     name="articlemeta",
-    version='1.16.3',
+    version='1.16.4',
     description="A SciELO API to load SciELO Articles metadata",
     author="SciELO",
     author_email="scielo-dev@googlegroups.com",
