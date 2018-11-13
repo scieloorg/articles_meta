b5deb2165f7074ecac0a1bb88a96adc98f288851
robertatakenaka <roberta.takenaka@scielo.org>
2018-11-13 14:07:28 -0200

    O fmt=opac deve retornar "fulltexts"
    Faz com que o "fulltexts" volte a ser retornado
    
    articlemeta/export.py
    
    ```
    from articlemeta.client import ThriftClient
    cl = ThriftClient()
    article = cl.document(code='S0074-02761929000100003', collection='scl', fmt='opac')
    article.data['fulltexts']
    
    ```
    
    Retorno esperado:
    
    ```json
    {u'pdf': {u'fr': u'http://www.scielo.br/pdf/mioc/v22n1/fr_tomo22(f1)_135-139.pdf', u'pt': u'http://www.scielo.br/pdf/mioc/v22n1/tomo22(f1)_135-139.pdf'}, u'html': {u'pt': u'http://www.scielo.br/scielo.php?script=sci_arttext&pid=S0074-02761929000100003&tlng=pt'}}
    ```
    
    n/a
    
    Fixes #149
    
    n/a
    
    n/a
    
    tk149

--- articlemeta/export.py
+++ articlemeta/export.py
@@ -221,7 +221,7 @@ class Export(object):
         article = self._article.copy()
         keys_to_remove = ['citations', '_shard_id', 'validated_scielo',
                 'doaj_id', 'normalized', 'sent_doaj', 'sent_wos',
-                'validated_wos', 'applicable', 'fulltexts']
+                'validated_wos', 'applicable']
 
         for k in keys_to_remove:
             try:
